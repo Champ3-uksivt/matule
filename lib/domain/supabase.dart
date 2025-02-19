54dd5d2bce4fc579563874c7305a6a154813ab86
uksivt <uksivt@iMac-uksivt-3.local>
2025-02-19 16:13:32 +0500

    regist singleton

--- lib/domain/supabase.dart
+++ lib/domain/supabase.dart
@@ -9,5 +9,6 @@ class Supabase_Init {
     var anonKey =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzcm11eHFoeXp0dGRzc2l4bmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2NzA2NzEsImV4cCI6MjA1NDI0NjY3MX0.beXgx7Z5RoM_P4pED20HWWEcBuR0wZFhn_7_I3EQxH4";
     var supa = await Supabase.initialize(url: supabaseUrl, anonKey: anonKey);
+    GetIt.I.registerSingleton(supa);
   }
 }
